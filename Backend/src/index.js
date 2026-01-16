524a45d2c152e64fb0c12566cbd027da0d8b0fde
Chethanreddyc <chethanreddy0503@gmail.com>
2026-01-16 18:59:23 +0530

    deploy commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
+import path from 'path'
 
 import noteRoutes from './routes/noteRoutes.js';
 import {connectDB} from './config/db.js';
@@ -11,16 +12,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;  
+const __dirname = path.resolve()
 
+
+if (process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin:"http://localhost:5173"
 }));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 
-app.use ('/api/notes',noteRoutes)
+app.use ('/api/notes',noteRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
